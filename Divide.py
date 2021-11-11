8b05d7367001be52850610781f464535df5b6260
OliviaFortuneUCD <81123269+OliviaFortuneUCD@users.noreply.github.com>
2021-11-11 20:23:24 +0000

    Add files via upload

---
+++ Divide.py
@@ -0,0 +1,19 @@
+# importing pandas library
+import pandas as pd
+
+# Initializing the nested list with Data-set
+player_list = [['M.S.Dhoni', 36, 75, 5428000],
+               ['A.B.D Villers', 38, 74, 3428000],
+               ['V.Kholi', 31, 70, 8428000],
+               ['S.Smith', 34, 80, 4428000],
+               ['C.Gayle', 40, 100, 4528000],
+               ['J.Root', 33, 72, 7028000],
+               ['K.Peterson', 42, 85, 2528000]]
+
+# creating a pandas dataframe
+df = pd.DataFrame(player_list,
+                  columns=['Name', 'Age',
+                           'Weight', 'Salary'])
+
+# show the dataframe
+print(df.Weight*10)
